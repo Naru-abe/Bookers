00f8824c82cea675407b70f53339b686bc16d8ff
EC2 Default User <ec2-user@ip-172-31-19-192.ap-northeast-1.compute.internal>
2022-06-06 13:20:22 +0000

    add form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,26 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice]= "Book was successfully created."
+      redirect_to book_path(book.id)
+    end
+  end
+
   def index
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
